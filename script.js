cf5a183aaa03086d271e85cfbb981b66e1093430
rehmanmustafasoomro800-sketch <rehmanmustafasoomro800@gmail.com>
2026-07-23 16:09:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+
+card.addEventListener("mouseenter", () => {
+card.style.boxShadow = "0 0 20px #00c3ff";
+});
+
+card.addEventListener("mouseleave", () => {
+card.style.boxShadow = "none";
+});
+
+});
+
+const btn = document.querySelector(".btn");
+
+if (btn) {
+btn.addEventListener("click", function () {
+alert("Welcome to S-Fiber Internet Service & IT Academy!");
+});
+}
+
+});
